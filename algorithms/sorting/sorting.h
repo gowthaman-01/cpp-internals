d1177ef15cb5a95133503f95e2c8d5e19158a253
Gowthaman <gowthaman169@gmail.com>
2025-04-04 11:10:30 +0800

    Modify quicksort function prototype

--- algorithms/sorting/sorting.h
+++ algorithms/sorting/sorting.h
@@ -8,5 +8,5 @@ void merge_sort(std::vector<int>& arr);
 void merge_sort(std::vector<int>& arr, int start, int end);
 void merge(std::vector<int>& arr, int start, int mid, int end);
 
-void quick_sort(std::vector<int> arr);
-void quick_sort(std::vector<int> arr, int start, int end);
+void quick_sort(std::vector<int>& arr);
+void quick_sort(std::vector<int>& arr, int start, int end);
